04f48a7d4f694e88ccd4a248cc0461185c3a63e9
Pinal Parmar <pinalparmar270@gmail.com>
2026-02-01 23:46:43 +0530

    render_template flask

---
+++ flask/getpostapp.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template,request
+
+app=Flask(__name__)
+
+@app.route('/')
+def main_pg():
+    return "<html><h1>Welcome to Flask App</h1></html>"
+
+@app.route('/index')
+def index_pg():
+    return render_template('index.html')
+
+@app.route('/about')
+def about_pg():
+    return render_template('about.html')
+
+@app.route('/form',methods=['GET','POST'])
+def form():
+    if request.method == 'POST':
+        name=request.form['name']
+        return f"hello {name}"
+    return render_template('form.html')
+if __name__=="__main__":
+    app.run(debug=True)
